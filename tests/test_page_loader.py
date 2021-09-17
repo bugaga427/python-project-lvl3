546418ccec6e4cd8523a11fd9a2ac541fcd539f0
Влад Гуштын <vladislav3372@gmail.com>
2021-09-17 18:14:23 +0300

    fix tests

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -7,7 +7,11 @@ def test_download_page():
     with open(path_to_expect) as expect:
         with TemporaryDirectory() as tmp:
             tmp_dir_name = tmp
-            file_path = download('https://ru.hexlet.io/courses', tmp_dir_name)
+            file_path = download(
+                'https://page-loader.hexlet.repl.co',
+                tmp_dir_name
+            )
             expect_content = expect.read()
             with open(file_path) as result:
-                assert expect_content == result.read()
+                result_content = result.read()
+                assert expect_content == result_content
