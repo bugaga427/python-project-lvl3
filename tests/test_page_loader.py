79d27e4a21407822425be2639dfabc5991cd3981
Влад Гуштын <vladislav3372@gmail.com>
2021-10-06 23:47:22 +0300

    fix tests

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,17 +1,36 @@
 from page_loader.downloader import download
 from tempfile import TemporaryDirectory
+import os.path
+import requests_mock
+
+path_to_expect_html = os.path.join(
+    'tests',
+    'fixtures',
+    'page-loader-hexlet-repl-co-courses_files',
+    'page-loader-hexlet-repl-co-courses.html'
+)
+path_to_expect_image = os.path.join(
+    'tests',
+    'fixtures',
+    'page-loader-hexlet-repl-co-courses_files',
+    'page-loader-hexlet-repl-co-assets-professions-nodejs.png'
+)
 
 
 def test_download_page():
-    path_to_expect = 'tests/fixtures/page-loader-hexlet-repl-co-courses.html'
-    with open(path_to_expect) as expect:
-        with TemporaryDirectory() as tmp:
-            tmp_dir_name = tmp
-            file_path = download(
+    with TemporaryDirectory() as tmp:
+        tmp_dir_name = tmp
+        with requests_mock.Mocker() as m:
+            m.get(
+                'https://page-loader.hexlet.repl.co/',
+                text=open(path_to_expect_html).read()
+            )
+            html_path = download(
                 'https://page-loader.hexlet.repl.co/',
                 tmp_dir_name
             )
+        with open(path_to_expect_html) as expect:
             expect_content = expect.read()
-            with open(file_path) as result:
-                result_content = result.read()
-                assert expect_content == result_content
+            with open(html_path) as html:
+                html_content = html.read()
+                assert expect_content == html_content
