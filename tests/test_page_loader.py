408155d8a1dee2ea9d256cdb4bd39ec9b23423f3
Влад Гуштын <vladislav3372@gmail.com>
2021-10-10 22:07:53 +0300

    update tests

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,4 +1,4 @@
-from page_loader.downloader import download
+from page_loader.downloader import download, download_image
 from tempfile import TemporaryDirectory
 import os.path
 import requests_mock
@@ -16,17 +16,20 @@ path_to_expect_image = os.path.join(
     'page-loader-hexlet-repl-co-assets-professions-nodejs.png'
 )
 
+html_url = 'https://page-loader.hexlet.repl.co/'
+image_url = 'https://page-loader.hexlet.repl.co/assets/professions/nodejs.png'
+
 
 def test_download_page():
     with TemporaryDirectory() as tmp:
         tmp_dir_name = tmp
         with requests_mock.Mocker() as m:
             m.get(
-                'https://page-loader.hexlet.repl.co/',
+                html_url,
                 text=open(path_to_expect_html).read()
             )
             html_path = download(
-                'https://page-loader.hexlet.repl.co/',
+                html_url,
                 tmp_dir_name
             )
         with open(path_to_expect_html) as expect:
@@ -34,3 +37,18 @@ def test_download_page():
             with open(html_path) as html:
                 html_content = html.read()
                 assert expect_content == html_content
+
+        with requests_mock.Mocker() as m:
+            m.get(
+                image_url,
+                content=open(path_to_expect_image, 'rb').read()
+            )
+        image_path = download_image(
+            image_url,
+            os.path.split(html_path)[0]
+        )
+        with open(path_to_expect_image, 'rb') as expect:
+            expect_content = expect.read()
+            with open(image_path, 'rb') as image:
+                image_content = image.read()
+                assert expect_content == image_content
